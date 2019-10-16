da08beb40784c9aae6009fbb76dc028250876f6d
Dana <dana.alibay@nu.edu.kz>
2019-10-16 21:07:45 +0600

    printalpabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,14 +1,11 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
-func main() {	
+func main() {
 	var i rune
-	for i = 97; i < 123; i++ { 
+	for i = 97; i <=122; i++{
 		z01.PrintRune(i)
 	}
-	
-	z01.PrintRune(10)
+	z01.PrintRune('\n')
 }
