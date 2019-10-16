aa18425932077043ea7eddf5c760b0e8be4c08d1
Dana <dana.alibay@nu.edu.kz>
2019-10-16 20:35:14 +0600

    my commit

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,10 @@
 package main
+
 import "github.com/01-edu/z01"
 
-func main(){
+func main() {
 	var i rune
-	for i =97; i<=122; i++{
+	for i =97; i <=122; i++{
 		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
