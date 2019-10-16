0fc48d863be3f55d285d98a654a3cb5e0007a707
Dana <dana.alibay@nu.edu.kz>
2019-10-16 22:17:18 +0600

    isn

--- isnegative.go
+++ isnegative.go
@@ -5,10 +5,8 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune(84)
-		z01.PrintRune(\n)
 	} else {
 		z01.PrintRune(70)
-		z01.PrintRune(\n)
 	}
-
+	z01.PrintRune(\n)
 }
