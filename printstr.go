734a5c6ac9ff69ca0c48e9c7adcc2e1c4abe6925
Dana <dana.alibay#nu.edu.kz>
2019-10-17 23:34:13 +0600

    m

--- printstr.go
+++ printstr.go
@@ -1,7 +1,10 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	fmt.Printf("%v", str)
+	for i = 0; i < 12; i++ {
+		j = str[i]
+		z01.PrintRune(j)
+	}
 }
