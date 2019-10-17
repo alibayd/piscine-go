a21abca8a7168c73e774f113da3856339ba150f3
Dana <dana.alibay#nu.edu.kz>
2019-10-17 23:45:00 +0600

    m

--- printstr.go
+++ printstr.go
@@ -3,10 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	var i rune
-	var j rune
-	for i = 0; i < 12; i++ {
-		j = str[i]
-		z01.PrintRune(j)
-	}
+	z01.PrintRune(rune(str[0]))
+	z01.PrintRune(rune(str[1]))
+	z01.PrintRune(rune(str[2]))
+	z01.PrintRune(rune(str[3]))
+	z01.PrintRune(rune(str[4]))
+	z01.PrintRune(rune(str[5]))
+	z01.PrintRune(rune(str[6]))
+	z01.PrintRune(rune(str[7]))
+	z01.PrintRune(rune(str[8]))
+	z01.PrintRune(rune(str[9]))
+	z01.PrintRune(rune(str[10]))
+	z01.PrintRune(rune(str[11]))
 }
