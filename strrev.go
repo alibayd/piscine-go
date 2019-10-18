5c8a6f626657e09442fbdbb7242388b60e1cafda
Dana <dana.alibay@nu.edu.kz>
2019-10-18 20:27:48 +0600

    r

--- strrev.go
+++ strrev.go
@@ -3,10 +3,17 @@ package piscine
 func StrRev(s string) string {
 	ru := []rune(s)
 	revru := []rune(s)
-	var i int = 11
-	for index := 0; index < 12; index++ {
-		revru[index] = ru[i]
-		i--
+
+	ar := []rune(s)
+	var len int
+	for k := range ar {
+		len = k 
+	}
+	var d int = len
+
+	for index := 0; index <= d; index++ {
+		revru[index] = ru[len]
+		len--
 	}
 	ss := string(revru)
 	return ss
