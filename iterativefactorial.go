141d391212764933433b89cdc9987d9d5a994fc8
Dana <dana.alibay@nu.edu.kz>
2019-10-21 15:10:12 +0600

    itfact

---
+++ iterativefactorial.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb > 20 || nb <= 0  {
+		return 0
+	}
+	var result int = 1
+	for i:= 1; i <= nb; i++ {
+		result=result*i  /// fr=fr*i
+	}
+	return result
+}
