4d184144d6aea80c141d51d0f2c4aee59fd6fa93
Dana <dana.alibay@nu.edu.kz>
2019-10-21 21:49:51 +0600

    fnp

--- findnextprime.go
+++ findnextprime.go
@@ -1,11 +1,11 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	for i := 2; i <= nb / 2; i++ {
+	for i := 2; i <= nb/2; i++ {
 		if nb%i == 0 {
 			nb += 1
 			i = 2
 		}
-	} 
+	}
 	return nb
 }
