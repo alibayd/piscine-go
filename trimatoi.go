450918772ce6dffbbe1d802cba2fed6c4f316d0b
Dana <dana.alibay@nu.edu.kz>
2019-10-23 02:11:41 +0600

    g

--- trimatoi.go
+++ trimatoi.go
@@ -4,20 +4,21 @@ package piscine
 // 	"fmt"
 // )
 
+
 func TrimAtoi(s string) int {
 	ru := []rune(s)
 	var myint int = 0
 	var only []int
 	var sn int = 1
-	for _, letter := range ru {
-		if letter == '-' && only == nil {
+	for _, letter := range ru{
+		if letter == '-' && only == nill{
 			sn = -sn
 		}
 		dg := 0
 		for j := '0'; j <= '9'; j++ {
 			// if only == nil && dg == 0 {
 			// 	break
-			// }else
+			// }else 
 			if letter == j {
 				only = append(only, dg)
 				break
@@ -30,5 +31,14 @@ func TrimAtoi(s string) int {
 	for i := range only {
 		len = i + 1
 	}
+	
+
+	for i := 0; i < len; i++ {
+		myint = myint*10 + only[i]
+	}
+	myint*=sn
+
+	return myint
+
 
 }
