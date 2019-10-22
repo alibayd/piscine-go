af9f5f5d0132e41a0a9ac57437887082e292eb51
Dana <dana.alibay@nu.edu.kz>
2019-10-23 02:42:26 +0600

    f

---
+++ index.go
@@ -0,0 +1,31 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	gv := []rune(s)
+	fd := []rune(toFind)
+	var count int 
+	var len int
+	for i := range fd {
+		len = i + 1
+	}
+	for _, i := range gv {
+		for _, j:= range toFind {
+			if i == j {
+				count++
+			} 
+		}
+	}
+	if count < len {
+		return -1                 // case when the given string is absent
+	}
+	if len == 1 {
+		for index, letter := range gv {
+			for _, letter1 := range fd {
+				if letter == letter1 {        // case when given string is one letter
+					return index
+				}
+			}
+		}
+	}
+	return 1
+}
