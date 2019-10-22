652a120de8a832a96d5679553a3c18463eaeb5f3
Dana <dana.alibay@nu.edu.kz>
2019-10-23 04:33:05 +0600

    dfvdcs

--- tolower.go
+++ tolower.go
@@ -3,7 +3,7 @@ package piscine
 func ToLower(s string) string {
 	ss := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if ss[i] >= 'a' && ss[i] <= 'z' {
+		if ss[i] >= 'A' && ss[i] <= 'Z' {
 			ss[i] = ss[i] + 32
 			//i++
 		}
