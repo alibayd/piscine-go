2de244f6967c83577c8f0af7e95c29d4784b2754
Dana <dana.alibay@nu.edu.kz>
2019-10-23 20:32:07 +0600

    revp

---
+++ revparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	var len int
+	for i := range os.Args {
+		len = i + 1
+	}
+	for i := len; i >= 1; i++ {
+		ar := []rune(os.Args[i])
+		for _, k := range ar {
+			z01.PrintRune(k)
+		}
+		z01.PrintRune('\n')
+	}
+}
