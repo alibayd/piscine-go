01a53f52e2dd4e35eb51984b4dbdefcfd3950043
Dana <dana.alibay#nu.edu.kz>
2019-10-23 10:43:20 +0600

    ccds

--- capitalize.go
+++ capitalize.go
@@ -2,7 +2,11 @@ package piscine
 
 func Capitalize(s string) string {
 	ar := []rune(s)
-	for i := 0; i < len(s); i++ {
+	var len int
+	for i := range ar {
+		len = i + 1
+	}
+	for i := 0; i < len; i++ {
 		if i == 0 || !((ar[i-1] >= 'A' && ar[i-1] <= 'Z') || (ar[i-1] >= 'a' && ar[i-1] <= 'z') || (ar[i-1] >= '0' && ar[i-1] <= '9')) {
 			if ar[i] >= 'a' && ar[i] <= 'z' {
 				ar[i] -= 32
