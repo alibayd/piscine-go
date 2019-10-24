83289b7741783593c47e3670cdba77f8b9769fa9
Dana <dana.alibay@nu.edu.kz>
2019-10-24 20:18:40 +0600

    cc

--- concatparams.go
+++ concatparams.go
@@ -3,12 +3,12 @@ package piscine
 func ConcatParams(args []string) string {
 	var len int
 	for i := range args {
-		len = i 
+		len = i
 	}
 
 	var res string
 
-	for i:= range args {
+	for i := range args {
 		res += args[i]
 		if i != len {
 			res += "\n"
