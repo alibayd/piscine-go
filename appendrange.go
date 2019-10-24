afaa42fe70148f91144b8b5efee797ab9c780822
Dana <dana.alibay@nu.edu.kz>
2019-10-24 19:42:15 +0600

    app

---
+++ appendrange.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min > max {
+		return nil
+	}
+	var ans []int
+	var rg int = max - min
+
+	for i := 0; i < rg; i++ {
+		ans = append(ans, min + i)
+	}
+	
+	return ans
+}
