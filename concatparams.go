e751489234c085945569184cb17735bc7c58bda0
Dana <dana.alibay@nu.edu.kz>
2019-10-24 20:17:47 +0600

    cc

---
+++ concatparams.go
@@ -0,0 +1,18 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	var len int
+	for i := range args {
+		len = i 
+	}
+
+	var res string
+
+	for i:= range args {
+		res += args[i]
+		if i != len {
+			res += "\n"
+		}
+	}
+	return res
+}
